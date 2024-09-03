300d0915a6e670fff5e7a9920e8ea0c1a719ca2e
Michael Hauser <michuhauser@gmail.com>
2024-09-03 07:57:18 -0700

    dockerization for native dev env

--- s81native/app/+not-found.tsx
+++ s81native/app/+not-found.tsx
@@ -1,8 +1,8 @@
+import * as React from 'react';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/old/ThemedText';
-import { ThemedView } from '@/components/old/ThemedView';
 
 export default function NotFoundScreen() {
   return (
